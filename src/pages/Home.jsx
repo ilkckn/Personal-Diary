32bf555917ccdc10ddc2f9c37b9d5dbe06d5d5ad
Ali <taspinara17@gmail.com>
2025-02-27 16:58:13 +0100

    created basic structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-amber-800 bg-amber-200 text-center py-4">Personal Diary</h1>
+        </div>
+    )
+}
+
+export default Home
